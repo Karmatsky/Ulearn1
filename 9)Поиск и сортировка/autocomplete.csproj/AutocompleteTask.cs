b83bc328c93336be1234131fe04b89338c441d66
Savely Karmatsky <88971699+Karmatsky@users.noreply.github.com>
2022-05-21 18:21:05 +0500

    Add files via upload

---
+++ 9)Поиск и сортировка/autocomplete.csproj/AutocompleteTask.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Autocomplete
+{
+    internal class AutocompleteTask
+    {
+        public static string FindFirstByPrefix(IReadOnlyList<string> phrases, string prefix)
+        {
+            var index = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1;
+            if (index < phrases.Count && phrases[index].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                return phrases[index];
+            
+            return null;
+        }
+
+        public static string[] GetTopByPrefix(IReadOnlyList<string> phrases, string prefix, int count)
+        {
+            var countByPrefix = Math.Min(count, GetCountByPrefix(phrases, prefix));
+            var left = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count);
+            var topByPrefix = new string[countByPrefix];
+            var c = 0;
+            while (c < countByPrefix)
+            {
+                topByPrefix[c] = phrases[left + c + 1];
+                c++;
+            }
+            return topByPrefix;
+        }
+
+        public static int GetCountByPrefix(IReadOnlyList<string> phrases, string prefix)
+        {
+            var phrasesNumber = phrases.Count;
+            var left = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrasesNumber);
+            var right = RightBorderTask.GetRightBorderIndex(phrases, prefix, -1, phrasesNumber);
+            return right - left - 1;
+        }
+    }
+
+    [TestFixture]
+    public class AutocompleteTests
+    {
+        [Test]
+        public void TopByPrefix_IsEmpty_WhenNoPhrases()
+        {
+            var phrases = new string[] { "aa", "ab", "bc", "bd", "be", "ca", "cb" };
+            var prefix = "aaa";
+            var expectedCount = 0;
+            var actualCount = AutocompleteTask.GetCountByPrefix(phrases, prefix);
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        // ...
+
+        [Test]
+        public void CountByPrefix_IsTotalCount_WhenEmptyPrefix()
+        {
+            var phrases = new string[] { "aa", "ab", "bc", "bd", "be", "ca", "cb" };
+            var prefix = "aaa";
+            var expectedCount = 0;
+            var actualCount = AutocompleteTask.GetCountByPrefix(phrases, prefix);
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        // ...
+    }
+}
